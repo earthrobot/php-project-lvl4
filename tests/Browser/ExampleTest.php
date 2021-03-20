7e0fab69882791d488cacadb62a6440b96be2369
earthrobot <syley-20@yandex.ru>
2021-03-20 17:00:08 +0400

    fix example tests

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends DuskTestCase
      */
     public function testBasicExample()
     {
-        $this->browse(function (Browser $browser) {
+        $this->browse(function (Browser $browser): void {
             $browser->visit('/')
                     ->assertSee('Laravel');
         });
